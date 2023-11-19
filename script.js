f9ad0b7b63eef70103e5af1cfc2808d183e9c6c4
tainaramos <151399884+tainaramos@users.noreply.github.com>
2023-11-19 12:39:34 -0300

    Calculadora

---
+++ script.js
@@ -0,0 +1,26 @@
+const button = document.getElementById('button');
+const nome = document.getElementById('nome');
+const peso = document.getElementById('peso');
+const altura = document.getElementById('altura');
+const resultado = document.getElementById('resultado');
+
+
+const getText = (imc) => {
+    if(imc > 40) return 'Obesidade grau III';
+    if(imc > 35) return 'Obesidade grau II';
+    if(imc > 30) return 'Obesidade grau I';
+    if(imc > 25) return 'Levemente acima do peso';
+    if(imc > 18.5) return 'Peso ideal';
+    if(imc < 18.5) return 'Abaixo do peso';
+
+}
+
+const imcCalc = () => {
+    if(!peso.value || !altura.value || !nome.value) return resultado.textContent = 'Preencha todos os campos';
+
+    const valorImc = (+peso.value / (+altura.value * +altura.value)).toFixed(1);
+    resultado.textContent = `${nome.value} = ${getText(valorImc)}`;
+
+}
+
+button.addEventListener('click', imcCalc);
